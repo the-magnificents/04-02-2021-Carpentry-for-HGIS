d5bf39f82fc9897d6910664c64d4964160d79b21
jurra <jose.urra86@gmail.com>
2021-02-16 14:56:02 +0100

    Algorithm partially working

--- 02_Python_Draw_GIS/polygon.py
+++ 02_Python_Draw_GIS/polygon.py
@@ -1,49 +1,63 @@
 import math
 
+def centroid(vertexes):
+     _x_list = [vertex [0] for vertex in vertexes]
+     _y_list = [vertex [1] for vertex in vertexes]
+     _len = len(vertexes)
+     _x = sum(_x_list) / _len
+     _y = sum(_y_list) / _len
+     return [_x, _y]
+
+
 def get_angle(coords):
     '''
     The function gets the angle based on a pair of coordinates x and y
     using basic trigonometry to calculate the angle of the triangle
     this angle will be used to sort coordinates of an array
-    '''
+    '''     
     # Compute hypothenuse
     hypothenuse = math.sqrt(coords[0] ** 2 + coords[1] ** 2) 
+    
     # the opposite side is y so we used the second value in coords list
     angle = math.degrees(math.asin(coords[1]/hypothenuse))
-    # print("Hypothenuse: " + str(hypothenuse))
-    # print("Angle: " + str(angle))
+    
+    if(coords[0] < 0):
+        angle = angle * 3
+
     if angle < 0:
         angle = 360 + (-angle)
     
     return angle
-    
 
-def sort_points(points):
-    '''
-    this functions sorts points from smaller to bigger angles to be able
-    to draw points from left to right and draw proper polygons
-    '''
-    reference = 1
-    def sort_item(pointer):
+def sort_item(ref,points):
         '''
-        It sorts the last element in a list starting from a reference pointer
+        It sorts the last element in a list starting from a reference ref
         iterating backwards, if previous value is bigger, then the last element
         moves one index down
         '''
-        while pointer > 0:
-            curr = get_angle(points[pointer])
-            prev = get_angle(points[pointer - 1])
+        while ref >= 0:
+            curr = get_angle(points[ref])
+            prev = get_angle(points[ref - 1])
             if curr < prev:
-                old_curr = points[pointer]
-                points[pointer] = points[pointer-1]
-                points[pointer-1] = old_curr
-                pointer -= 1
+                old_curr = points[ref]
+                points[ref] = points[ref-1]
+                points[ref-1] = old_curr
+                ref -= 1
             else:
                 break
+        
+        return points
+
+def sort_points(points):
+    '''
+    this function sorts points from smaller to bigger angles to be able
+    to draw points from left to right and draw proper polygons
+    '''
+    reference = 1
 
     
     while reference < len(points):
-        sort_item(reference)
+        points = sort_item(reference, points)
         reference += 1
 
     
@@ -62,8 +76,11 @@ def get_next_pt(points):
     
     return next
 
-# print(get_next_pt([[10,20],[100,20],[-10,40]]))
 
+print(get_angle([-142.5,115]))
+print(get_angle([7.5,135.0]))
 
-# print(get_angle([300,-150]))
-# sort_points([[10,20], [-90,80], [-100,90],[50,60]])
+# Example
+points = [[-142.5, 115.0], [7.5, 135.0], [-72.5, -35.0], [207.5, -215.0]]
+print(points)
+print(sort_points(points)) 
